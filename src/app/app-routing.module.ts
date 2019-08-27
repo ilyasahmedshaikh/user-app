667deb40d946c35c4e0edcc79ae91bc580e1c712
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2019-08-27 11:48:01 -0700

    addign routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SearchPageComponent } from './components/search-page/search-page.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'search', component: SearchPageComponent },
+  { path: 'about', component: AboutComponent },
+
+  // default route
+  { path: '',   redirectTo: '/search', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
